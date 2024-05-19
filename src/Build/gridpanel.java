e714e9b880c816bb9e79f4eac80f2bb0e6cbda90
tthehuman <miket5279@gmail.com>
2024-05-18 17:52:19 -0700

    starting over

--- src/Build/gridpanel.java
+++ src/Build/gridpanel.java
@@ -21,8 +21,6 @@ public class gridpanel extends JPanel implements ActionListener, KeyListener{
     static double VelX;
     static double VelY;
 
-    static int pdx;
-    static int pdy;
     int width = 10;
     int height = 10;
     int vel;
@@ -31,16 +29,18 @@ public class gridpanel extends JPanel implements ActionListener, KeyListener{
     static double pi2 = Math.PI/2;
     static double pi3 = 3 * Math.PI/2;
     private double speed;
-    private int squareCenterX = 200;
-    private int squareCenterY = 200;
+    int mapX = 8;
+    int mapY = 8;
+
     private static double angle = 0;
     static int pa = (int) angle;
-    @SuppressWarnings("unused")
     private static double maxMovementSpeed = 0.1;
     private double minSpeed;
     private double maxSpeed;
     static double dr = 0.0174533;
     static double movementSpeed = Math.sqrt(VelX + VelY);
+
+    static Rectangle Recta;
 
     
     
@@ -48,10 +48,12 @@ public class gridpanel extends JPanel implements ActionListener, KeyListener{
 
     Timer tm = new Timer(5, this);
 
+
+
 //----------------------------//
 
-    static int x;
-    static int y;
+    static int px = 200;
+    static int py = 200;
     
 //---------------------------------//
 
@@ -69,63 +71,7 @@ public class gridpanel extends JPanel implements ActionListener, KeyListener{
 
 
 
-//------------squares------------//
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        Graphics2D g3 = (Graphics2D) g;
 
-        for (int i =  0; i < grid.length; i++) {
-            for (int j =  0; j < grid[i].length; j++) {
-                if (grid[i][j] ==  1) {
-                    g2d.setColor(Color.BLACK);
-                } else {
-                    g2d.setColor(Color.WHITE);
-                }
-                g2d.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
-            
-            }
-        }
-        
-        AffineTransform transform = new AffineTransform();
-        transform.rotate(angle, squareCenterX, squareCenterY);
-
-        AffineTransform oldTransform = g3.getTransform();
-
-        g3.transform(transform);
-
-        g3.setColor(Color.GRAY);
-        g3.fillRect(squareCenterX - 8, squareCenterY - 8, 16, 16); 
-
-        g3.setTransform(oldTransform);
-
-        angle += speed;
-   
-    }
-//-------------------------------//
-
-
-//------------angle speed check, use later------------//
-    @SuppressWarnings("unused")
-    private double speed() {
-        if (angle < minSpeed) return minSpeed;
-        if (angle > maxSpeed) return maxSpeed;
-        return angle;
-    }
-//---------------------------------------------------//
-
-
-//------------action and repaint set up------------//
-    public void actionPerformed(ActionEvent e){
-
-
-        squareCenterX = (int) (squareCenterX + VelX);
-        squareCenterY = (int) (squareCenterY + VelY);
-
-        repaint();
-    }
-//-------------------------------------------------//
 
 
 //------------keybinds !!!------------//    
@@ -192,123 +138,71 @@ public class gridpanel extends JPanel implements ActionListener, KeyListener{
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) speed = 0;
     }
 //------------------------------------//    
-static float dist(float ax, float ay, float bx, float by, float ang){
-    return ( (bx - ax) * (bx - ax) + (by - ay) * (by - ay));
-}
 
 
-//------------raycaster-----------//
-static void raycaster(){
-    int r, mx, my, mp, dof;
-    float rx = x, ry = y, ra, xo = 0, yo = 0, distT;
 
-    ra = (float) ((float) angle - dr * 30);
+//-------------------------------//
 
-    if(ra<0){  ra += 2 * Math.PI;  }
-    if(ra>2 * Math.PI){  ra -= 2 * Math.PI;  }
 
-    for(r = 0; r<60; r++){
-        dof = 0;
-        float distH = 100000000, hx = x, hy = y;
-        float aTan = (float) (-1/Math.tan(ra));
+//------------squares------------//
+@Override
+public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g;
+    Graphics2D g3 = (Graphics2D) g;
 
-        if(ra>Math.PI){
-            ry = (float) (( ( (int) y >>6 ) >> 6) - 0.0001); 
-            rx = (y-ry) * aTan + x; 
-            yo = -64; 
-            xo = yo * aTan;
-        }
-        
-        if(ra<Math.PI){
-            ry = (float) (( ( (int) y >>6 ) >> 6) + 64); 
-            rx = (y-ry) * aTan + x; 
-            yo = 64; 
-            xo = yo * aTan;  
-        }
-
-        if(ra==0 || ra==Math.PI){
-            rx = x; 
-            ry=y; 
-            dof = 8;   
-        }
-
-        while(dof < 8){
-            mx = (int) (rx) >> 6; 
-            my = (int) (ry) >> 6; 
-            mp = my * x + mx;
-            if(mp > 0 && mp < x * y && grid[my][mx] == 1) {
-                dof = 8;
-                hx = rx;
-                hy = ry;
-                distH = dist(x, y, hx, hy, ra);
-            } 
-            else {
-                rx += xo;
-                ry = yo; 
-                dof += 1; 
+    for (int i =  0; i < grid.length; i++) {
+        for (int j =  0; j < grid[i].length; j++) {
+            if (grid[i][j] ==  1) {
+                g2d.setColor(Color.BLACK);
+            } else {
+                g2d.setColor(Color.WHITE);
             }
-        }
-//-----------------------//
-
-        dof = 0;
-        distT = 0;
-        float distV = 100000000, vx = x, vy = y;
-        float nTan = (float) (-1/Math.tan(ra));
-
-        if(ra > pi2 && ra < pi3){
-            rx = (float) (( ( (int) x >>6 ) >> 6) - 0.0001); 
-            ry = (x-rx) * nTan + y; 
-            yo = -64; 
-            xo = yo * nTan;  
-        }
+            g2d.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
         
-        if(ra < pi2 || ra > pi3){
-            rx = (float) (( ( (int) x >>6 ) >> 6) + 64); 
-            ry = (x-rx) * nTan + y; 
-            yo = 64; 
-            xo = yo * nTan;  
         }
-
-        if(ra==0 || ra==Math.PI){
-            rx = x; 
-            ry = y; 
-            dof = 8;   
-        }
-
-        while(dof < 8){
-            mx = (int) (rx) >> 6; 
-            my = (int) (ry) >> 6; 
-            mp = my * x + mx;
-            if(mp > 0 && mp < x * y && grid[my][mx] == 1) {
-                dof = 8;
-                vx = rx;
-                vy = ry;
-                distV = dist(x, y, vx, vy, ra);
-            } 
-            else {
-                rx += xo;
-                ry = yo; 
-                dof += 1; 
-            }
-        }
-        if(distV<distH){  rx = vx; ry = vy; distT = distV;  }
-        if(distH<distV){  vx = hx; vy = hy; distT = distH;  }
-
-        float lineH = (squareSize * 320) / distT;
-
-        if(lineH > 320){  lineH = 320;  }
-
-
-
-
-
-        ra += dr;
-        if(ra<0){  ra += 2 * Math.PI;  }
-        if(ra>2 * Math.PI){  ra -= 2 * Math.PI;  }
     }
+    
+    AffineTransform transform = new AffineTransform();
+    transform.rotate(angle, px, py);
+
+    AffineTransform oldTransform = g3.getTransform();
+
+    g3.transform(transform);
+
+    g3.setColor(Color.GRAY);
+    g3.fillRect(px - 8, py - 8, 16, 16); 
+
+    g3.setTransform(oldTransform);
+
+    angle += speed;
+
+    
+
 }
 //-------------------------------//
 
+
+//------------angle speed check, use later------------//
+@SuppressWarnings("unused")
+private double speed() {
+    if (angle < minSpeed) return minSpeed;
+    if (angle > maxSpeed) return maxSpeed;
+    return angle;
+}
+//---------------------------------------------------//
+
+
+//------------action and repaint set up------------//
+public void actionPerformed(ActionEvent e){
+
+
+    px = (int) (px + VelX);
+    py = (int) (py + VelY);
+
+    repaint();
+}
+//-------------------------------------------------//
 
 //------------map and frame setup------------//
     public static void main(String[] args) {
@@ -333,8 +227,11 @@ static void raycaster(){
 
         frame.setVisible(true);
 
-        raycaster();
 
+        if(movementSpeed >= maxMovementSpeed){
+            VelX = maxMovementSpeed;
+            VelY = maxMovementSpeed;
+        }
     }
 }
 //----------------------------------------//
